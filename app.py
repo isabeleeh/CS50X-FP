9e881ce4239f44d815bb412ac848ca0d0bfe14ce
Isabeleeh <isabeleeh@gmail.com>
2022-12-14 15:48:40 +1100

    Initialize final project

---
+++ app.py
@@ -0,0 +1,10 @@
+import os, time
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
