de2417d144c53badc0070e522c7422fb72938ac3
Isabeleeh <isabeleeh@gmail.com>
2023-01-19 15:10:15 +1100

    Add the feature of 'read presctiption and write the data into db'

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, flash, redirect, render_template, request
 from werkzeug.utils import secure_filename
 from pypinyin import lazy_pinyin
 
-from helpers import apology, allowed_file, get_words
+from helpers import apology, allowed_file, split_herbs
 
 UPLOAD_FOLDER = './uploads'
 
@@ -15,6 +15,7 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = 'super secret key'
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///myherbs.db")
@@ -32,7 +33,7 @@ def after_request(response):
 @app.route("/")
 def index():
     """Show portfolio of all herbs"""
-    rows = db.execute("SELECT * FROM herbs")
+    rows = db.execute("SELECT * FROM herbs ORDER BY pinyin")
 
     if rows:
         return render_template("index.html", rows=rows)
@@ -42,7 +43,7 @@ def index():
 
 @app.route("/importfile", methods = ["GET", "POST"])
 def importfile():
-    """Import .csv or excel file which includes herbs inventory and write into herbs.db"""
+    """Import .csv file which includes herbs inventory and write into herbs.db"""
     if request.method == "POST":
         # User reaches here via "POST"
         
@@ -50,7 +51,7 @@ def importfile():
             flash('No file part')
             return redirect("/")
 
-            # Read the attachment content into a var myherbs as dict type
+            
         file = request.files['file']
         if file.filename == '':
             flash('No selected file')
@@ -62,20 +63,20 @@ def importfile():
             file.save(filepath)
             # return redirect(url_for('uploaded_file', filename=filename))
 
+            # Read the attachment content into a var myherbs as dict type
             # encoding='utf-8-sig' to remove the BOM /ufeff
             with open(filepath, "r", encoding='utf-8-sig') as csv_file:
                 reader = csv.DictReader(csv_file)
                 for row in reader:
                     
                     if row["Amount"] == '':
-                        amount = 0;
+                        amount = 100;
                     else:
                         amount = int(row["Amount"])
                     mixedname = row["Name"]
                     # Handle exceptions such as row in excel consists of character like ""
                     if mixedname == "":
                         continue
-                    logging.info(row["Amount"])
                     chinese = ''
                     # Fetch the Chinese character from a string
                     chinese = chinese.join(re.findall('[\u4e00-\u9fa5]', mixedname))
@@ -98,3 +99,80 @@ def importfile():
         return render_template("importfile.html")
 
     return redirect("/")
+
+@app.route("/prescription", methods = ["GET", "POST"])
+def prescription():
+    """ Handle the prescription from the doctor and minus the amount of each herb from db"""
+    if request.method == "POST":
+        prescript = request.form.get("prescript")
+        doses = request.form.get("doses")
+        if not doses:
+            doses = 3
+        else:
+            doses = int(doses)
+
+        # divide the prescription into a list
+        if not prescript or not doses:
+            return apology("prescription or doses cannot be blank")
+        # split_herbs function handles split and related scenarios
+        herbs_list = split_herbs(prescript)
+        
+        # To store the herbs that are not in db
+        invalid_herbs = []
+        # To store the herbs that have no adequet amount in db
+        insufficient_herbs = []
+        
+        for herb in herbs_list:            
+            item_db = db.execute("SELECT * FROM herbs WHERE Chinese_ch = ?", herb["Chinese_ch"])
+            # If cannot find the corresponding herb in db
+            if len(item_db) != 1:
+                invalid_herbs.append(herb)
+                continue
+            # if the amount in db is not sufficient for this time
+            #amount = int(amount)
+            amount_db = item_db[0]["amount"]
+            if (amount_db - herb["amount"] * doses) < 0:
+                insufficient_herbs.append(herb)
+                continue
+
+            db.execute("UPDATE herbs SET amount = ? WHERE Chinese_ch = ?", (amount_db - herb["amount"] * doses), herb["Chinese_ch"])
+
+        if invalid_herbs or insufficient_herbs:
+            # Show the listed herbs that have not been handled
+            return render_template("prescription.html", invalid_herbs = invalid_herbs, insufficient_herbs = insufficient_herbs)
+
+        #flash("Succeeded!")
+        
+
+    else:
+        return render_template("prescription.html")
+
+    return redirect("/")
+
+@app.route("/manage", methods = ["POST"])
+def manage():
+    # Shortcut to manage amount in db on "index" page
+    if request.method == "POST":
+        amount = request.form.get("quantity")
+        item_id = request.form.get("id")
+        if not amount:
+            apology("The amount cannot be blank")
+        amount = int(amount)
+        item_db = db.execute("SELECT * FROM herbs WHERE id = ?", item_id)
+        if (item_db[0]["amount"] + amount) < 0:
+            flash("Insufficient amount!")
+            return redirect("/")
+        
+        db.execute("UPDATE herbs SET amount = ? WHERE id = ?", (item_db[0]["amount"] + amount), item_id)
+
+        flash("Succeeded!")
+        return redirect("/")
+
+@app.route("/testonly", methods = ["GET", "POST"])
+def testonly():
+    if request.method == "POST":
+        # SUDO
+        i = request.form.get("id")
+    else:
+        
+        return render_template("test.html")
--- helpers.py
+++ helpers.py
@@ -1,4 +1,4 @@
-import os, string
+import os, string, re
 import requests
 import urllib.parse
 
@@ -6,6 +6,7 @@ from flask import redirect, render_template, request, session
 from functools import wraps
 
 ALLOWED_EXTENSIONS = {'csv'}
+EASILY_COMBINDED = {'生龙骨', '生牡蛎', '煅龙骨', '煅牡蛎', '石决明'}
 
 
 def apology(message, code=400):
@@ -26,9 +27,41 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def get_words(text):
-    words = []
-    for word in text.split():
-        if word.isalpha():
-            words.append(word)
-    return " ".join(words)
+def preprocess(text):
+    # To change "龙骨" "牡蛎" to "生龙骨" "生牡蛎"
+    tmp = text
+    if ("龙骨" in text) and ("生龙骨" not in text) and ("煅龙骨" not in text):
+        tmp = tmp.replace("龙骨", "生龙骨")
+    if ("牡蛎" in text) and ("生牡蛎" not in text) and ("煅牡蛎" not in text):
+        tmp = tmp.replace("牡蛎", "生牡蛎")
+    return tmp
+
+def split_herbs(prescript):
+    # To take the whole prescription as input, and return a dict type with herb name and amount.
+    # issue to solve: "牡蛎" and "煅牡蛎" could cause conflict
+    rough_list = prescript.split("，") # a rough list, probably include information like "生龙骨煅牡蛎各30克先煎" "柴胡12克"
+    item_list = []
+    for item in rough_list:
+        tmp = re.split(r"克", item)[0] # "生龙骨煅牡蛎各30克先煎" -> "生龙骨煅牡蛎各30"
+        amount = "".join(list(filter(str.isdigit, tmp))) # Catch the amount value
+        if amount:
+            amount = int(amount)
+
+        if '各' in tmp:
+            # like "生龙骨煅牡蛎各30"
+            tmp = re.split(r"各", item)[0] # "生龙骨煅牡蛎各30" -> "生龙骨煅牡蛎"
+            tmp = preprocess(tmp)
+            for chinese in EASILY_COMBINDED:
+                if tmp == "":
+                    break
+                if chinese in tmp:
+                    item_list.append({'Chinese_ch': chinese, 'amount': amount})
+                    tmp = tmp.replace(chinese, '')
+
+        else:
+            
+            # Catch the herb's Chinese name
+            chinese = ''
+            chinese = chinese.join(re.findall('[\u4e00-\u9fa5]', tmp)) 
+            item_list.append({'Chinese_ch': chinese, 'amount': amount})
+    return item_list   
